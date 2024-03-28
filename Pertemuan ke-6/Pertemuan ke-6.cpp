f759fc2a136ea68ca87715329ebcbc40de4f3944
ASUS <faris13abiyu@gmail.com>
2024-03-28 17:26:58 +0700

    Keterangan int

--- Pertemuan ke-6/Pertemuan ke-6.cpp
+++ Pertemuan ke-6/Pertemuan ke-6.cpp
@@ -4,8 +4,8 @@
 #include <iostream>
 using namespace std;
 
-
+//array of integers to hold values
 int arr[20];
-int cmp_count = 0;
-int mov_count = 0;
+int cmp_count = 0; // number of comparasion
+int mov_count = 0; // number of data movement
 int n;
